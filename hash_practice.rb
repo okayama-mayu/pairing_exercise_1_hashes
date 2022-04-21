3b8fc84ce8311c2413b22309d9ae1ec1579cac30
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-04-21 18:02:30 -0500

    Add hash persons

---
+++ hash_practice.rb
@@ -0,0 +1,14 @@
+persons = {
+  "hair_color" => "black",
+  "height" => "5 ft 3 in",
+  "weight" => 155,
+  "astrological_sign" => "leo"
+}
+puts persons
+
+# puts person["weight"]
+# puts persons["weight"] + persons[155]
+# puts "#{key}:#{value}"
+
+key, value = persons.first
+puts persons.first
